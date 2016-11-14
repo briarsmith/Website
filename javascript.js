7c3265d35398c529b9cada75e08538534e606b9a
briarsmith <briarsmith1@hotmail.com>
2016-11-13 21:45:37 -0500

    loop for home page broken

--- javascript.js
+++ javascript.js
@@ -1,3 +1,23 @@
 /**
  * Created by Briar on 11/13/2016.
  */
+
+// Cycle through background images
+
+window.setInterval(function () {
+    $("#home-background").delay(2000);
+    $("#home-background").attr('src', 'imgs/kittty.jpg');//.fadeOut(500);
+    $("#home-background").attr('src', 'imgs/space1.jpg').fadeIn(500);
+    $("#home-background").delay(2000);
+    $("#home-background").attr("src", "imgs/space1.jpg").fadeOut(500);
+    $("#home-background").attr("src", "imgs/nice tree.jpg").fadeIn(500);
+    $("#home-background").delay(2000);
+    $("#home-background").attr("src", "imgs/nice tree.jpg").fadeOut(500);
+    $("#home-background").attr("src", "imgs/tiger.jpg").fadeIn(500);
+    $("#home-background").delay(2000);
+    $("#home-background").attr("src", "imgs/tiger.jpg").fadeOut(500);
+    $("#home-background").attr("src", "imgs/kittty.jpg").fadeIn(500);
+    $("#home-background").attr('src', 'imgs/kittty.jpg');
+    
+}, 9000);
+
