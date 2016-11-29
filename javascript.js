c726da442a01dff680f6f48ea538d9cf6c1dd556
briarsmith <briarsmith1@hotmail.com>
2016-11-28 19:33:37 -0500

    finished the background slider, images width only adjusts and images cut off at the bottom of the page.

--- javascript.js
+++ javascript.js
@@ -2,19 +2,21 @@
  * Created by Briar on 11/13/2016.
  */
 
-// Cycle through background images
 function cycleImages(){
     var $active = $('#cycler .active');
-    var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
+    var $next = ($active.next().not('.base').length > 0) ? $active.next().not('.base') : $('#cycler div').not('.base').first();
     $next.css('z-index',2);//move the next image up the pile
-    $active.fadeOut(3000,function(){//fade out the top image
+    $active.fadeOut(1500,function(){//fade out the top image
         $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
         $next.css('z-index',3).addClass('active');//make the next image the top one
     });
 }
 
 $(document).ready(function(){
-// run every 7s
-    setInterval('cycleImages()', 7000);
+    $('#cycler div.base').clone().prependTo('#cycler');
+    $('#cycler div.base').last().removeClass('base').addClass('active');
+    $('#cycler div').show();
+// run every 4s
+    setInterval('cycleImages()', 4000);
 })
 
