4805fe8035faf608a213f3e4e67b9b9d32947cc6
briarsmith1 <briar.smith@mozzaz.com>
2016-12-15 16:02:08 -0500

    delightful changes

--- js/javascript.js
+++ js/javascript.js
@@ -2,22 +2,31 @@
  * Created by Briar on 11/13/2016.
  */
 
-function cycleImages(){
-    var $active = $('#cycler .active');
-    var $next = ($active.next().not('.base').length > 0) ? $active.next().not('.base') : $('#cycler div').not('.base').first();
-    $next.css('z-index',2);//move the next image up the pile
-    $active.fadeOut(1500,function(){//fade out the top image
-        $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
-        $next.css('z-index',3).addClass('active');//make the next image the top one
+// Background image slider :) it works nicely 
+$(function() {
+    $('#maximage').maximage({
+        cycleOptions: {
+            fx: 'fade',
+            //sync: false,
+            speed: 1000,
+            timeout: 4000,
+            prev: '#arrow_left',
+            next: '#arrow_right',
+            pager: '#cycle-nav ul',
+            pagerAnchorBuilder: function(idx, slide) {
+                return '<li><a href="#"></a></li>';
+            }
+        },
+        onFirstImageLoaded: function() {
+            jQuery('#maximage').fadeIn('fast');
+        }
     });
-}
+    
+    $('.toggle').bind('click', function(e){
+        e.preventDefault();
+        $('#maximage').cycle('fx: fade');
+    });
+});
 
-$(document).ready(function(){
-    $('#cycler div.base').clone().prependTo('#cycler');
-    $('#cycler div.base').last().removeClass('base').addClass('active');
-    $('#cycler div').show();
-// run every 4s
-    setInterval('cycleImages()', 4000);
-})
 
 
