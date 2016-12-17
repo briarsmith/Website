3211ba900607774184ea9dc7cf2089cc56fda08d
briarsmith <briarsmith1@hotmail.com>
2016-12-16 23:13:56 -0500

    changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,9 @@
 /**
  * Created by Briar on 11/13/2016.
  */
+// $('#menu-icon').show();
+
+// $('#navyy').hide();
 // Background image slider :) it works nicely 
 $(function() {
     $('#maximage').maximage({
@@ -15,7 +18,26 @@ $(function() {
             }
         },
         onFirstImageLoaded: function() {
-            jQuery('#maximage').fadeIn('fast');
+            jQuery('#maximage').fadeIn(0);
+
+            $('#navyy').hide();
+            $('#menu-icon').show();
+            $('#menu-icon').on('click', function() {
+                $('#navyy').show();
+                $('#menu-icon').hide();
+            });
+
+            $(document).mouseup(function (e)
+            {
+                var container = $("#navyy");
+
+                if (!container.is(e.target) // if the target of the click isn't the container...
+                    && container.has(e.target).length === 0) // ... nor a descendant of the container
+                {
+                    $('#menu-icon').show();
+                    container.hide();
+                }
+            });
         }
     });
     
@@ -25,7 +47,7 @@ $(function() {
     });
 });
 
-//$('#navyy').hide();
+
 
 
 
